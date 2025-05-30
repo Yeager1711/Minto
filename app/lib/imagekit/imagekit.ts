721ee8f7827b5f818455345a83c7200c0d446830
Yeager1711 <huynhnam171102@gmail.com>
2025-05-30 11:38:52 +0700

    fix env

--- app/lib/imagekit/imagekit.ts
+++ app/lib/imagekit/imagekit.ts
@@ -2,8 +2,8 @@
 import ImageKit from 'imagekit-javascript';
 
 const imagekit = new ImageKit({
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY || '',
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || '',
+    publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || '',
+    urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT || '',
 });
 
 export default imagekit;
