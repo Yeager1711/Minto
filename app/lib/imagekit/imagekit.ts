635431fb67b0e78ff0059e8beac03cf3e7e5e7de
Yeager1711 <huynhnam171102@gmail.com>
2025-05-30 12:01:19 +0700

    add file

--- app/lib/imagekit/imagekit.ts
+++ app/lib/imagekit/imagekit.ts
@@ -2,8 +2,8 @@
 import ImageKit from 'imagekit-javascript';
 
 const imagekit = new ImageKit({
-    publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || '',
-    urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT || '',
+    publicKey: 'public_MWz6Jz8dBiCgIo0k5lWmcvBZjqk=',
+    urlEndpoint: 'https://ik.imagekit.io/zawkrzrax',
 });
 
 export default imagekit;
