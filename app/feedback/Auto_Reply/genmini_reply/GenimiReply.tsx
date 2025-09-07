adc08d1500888245186d58d5962f6073c4d16111
Yeager1711 <huynhnam171102@gmail.com>
2025-09-07 15:17:53 +0700

    add file

--- app/feedback/Auto_Reply/genmini_reply/GenimiReply.tsx
+++ app/feedback/Auto_Reply/genmini_reply/GenimiReply.tsx
@@ -330,7 +330,7 @@ const GeminiReply: React.FC<GeminiReplyProps> = ({ onClose }) => {
                         {messages.length === 0 && (
                             <div className={`${styles.message} ${styles.botMessage}`}>
                                 <div className={styles.messageContent_bot_intro}>
-                                    Hello, <br /> how may I assist you?
+                                    Anh/Chị <br /> Cần em hỗ trợ gì ?
                                     <div className={styles.loader}>
                                         <div
                                             className={`${styles.loader__inner} ${styles['loader__inner--one']}`}
